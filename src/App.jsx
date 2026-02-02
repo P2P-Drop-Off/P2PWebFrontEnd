074244ec70690ae57f2b166cb92f9eb0633decc2
Sienna Baird <siennatanner21@icloud.com>
2026-02-01 18:49:32 -0800

    partner application form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Map from './pages/Map'
 import CreateAccount from './pages/CreateAccount'
 import LogIn from './pages/LogIn'
 import Home from './pages/Home'
+import PartnerForm from './pages/PartnerForm'
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
 
       <Route path="/login" element={<LogIn />} />
       <Route path="/create-account" element={<CreateAccount />} />
+      <Route path="/partner-form" element={<PartnerForm />} />
     </Routes>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ export default function Header() {
       </div>
 
       <nav className="top-nav">
+        <a onClick={() => navigate("/partner-form")}>Partner Application</a>
         <a onClick={() => navigate("/create-listing")}>Create a Listing</a>
         <a onClick={() => navigate("/terms")}>Terms of Service</a>
 
